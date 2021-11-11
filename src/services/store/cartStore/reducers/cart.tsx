c54d62c8b2759010966bc36e4ae35c3602b9df0d
igor <hotap70390@bio123.net>
2021-11-11 12:54:51 +0300

    REDUX

--- src/services/store/cartStore/reducers/cart.tsx
+++ src/services/store/cartStore/reducers/cart.tsx
@@ -2,11 +2,13 @@ import uuid from "uuid";
 import produce from "immer";
 
 import {
-  ADD_QUANTITY,
+  ADD_ALL_ITEMS_TO_CART,
   ADD_TO_CART,
-  MINUS_QUANTITY,
   REMOVE_ALL_FROM_CART,
+  REMOVE_ITEM_FROM_CART,
+  UPDATE_INGREDIENTS,
   UPDATE_ITEMS_FROM_CART,
+  UPDATE_QUANTITY,
 } from "../../../../actions/types";
 
 const checkIngEquality = (a, b) => {
@@ -16,11 +18,11 @@ const checkIngEquality = (a, b) => {
 };
 
 const removeItem = (state, action) => {
-  let copiedState = { ...state };
-  const index = copiedState.cartItems.findIndex(
-    ({ id }) => id === action.data.id
-  );
-  let updatedCartItems = produce(copiedState, (draft) => {
+  console.log("STATEREMOVE", state);
+  console.log("ACTIONREMOVE", action);
+
+  const index = state.cartItems.findIndex(({ id }) => id === action.id);
+  let updatedCartItems = produce(state, (draft) => {
     draft.cartItems.splice(index, 1);
   });
   return {
@@ -29,15 +31,17 @@ const removeItem = (state, action) => {
   };
 };
 
-const changeQuantity = (state, action, modifier) => {
+const changeQuantity = (state, action) => {
+  console.log("STATE", state);
+  console.log("ACTION", action);
+
   let copiedState = { ...state };
-  const index = copiedState.cartItems.findIndex(({ id }) => id === action.id);
+  const index = copiedState.cartItems.findIndex(
+    ({ id }) => id === action.data.id
+  );
   let updatedCartItems = produce(copiedState, (draft) => {
-    const newQuantity = modifier(draft.cartItems[index].quantity);
+    const newQuantity = action.data.quantity;
     draft.cartItems[index].quantity = newQuantity;
-    if (newQuantity === 0) {
-      draft.cartItems.splice(index, 1);
-    }
   });
   return {
     ...updatedCartItems,
@@ -59,7 +63,7 @@ const updateIng = (state, action) => {
 
 const totalSum = (cartItems) => {
   return cartItems
-    .map((el) => el.price * el.quantity)
+    .map((el) => el.dish.price * el.quantity)
     .reduce((el1, el2) => {
       return el1 + el2;
     }, 0);
@@ -70,22 +74,19 @@ const InitialState = {
   totalSum: 0,
 };
 
-const addToCart = (state, action, quantity = 1) => {
+const addToCart = (state, action) => {
+  console.log("ACTIONA", action);
+
   let updatedCartItems = state;
   const itemIndex = state.cartItems.findIndex(
-    (item) =>
-      item.name === action.data.name &&
-      checkIngEquality(item.excludedIng, action.data.excludedIng)
+    (item) => item.id === action.data.id
   );
   if (itemIndex >= 0) {
     updatedCartItems = produce(state, (draft) => {
-      draft.cartItems[itemIndex].quantity += quantity;
+      draft.cartItems.splice(itemIndex, 1, action.data);
     });
   } else {
-    const newCartItems = [
-      ...state.cartItems,
-      { ...action.data, quantity: quantity, id: uuid.v4() },
-    ];
+    const newCartItems = [...state.cartItems, action.data];
     updatedCartItems = {
       ...updatedCartItems,
       cartItems: newCartItems,
@@ -99,15 +100,24 @@ const addToCart = (state, action, quantity = 1) => {
 
 export const Cart = (state = InitialState, action) => {
   switch (action.type) {
+    case ADD_ALL_ITEMS_TO_CART:
+      console.log(action, "ADD_ALL_ITEMS_TO_CART");
+      return { cartItems: action.data, totalSum: totalSum(action.data) };
     case ADD_TO_CART:
       return addToCart(state, action);
-
+    case REMOVE_ITEM_FROM_CART:
+      return removeItem(state, action);
     case REMOVE_ALL_FROM_CART:
       return InitialState;
-    case ADD_QUANTITY:
-      return changeQuantity(state, action, (oldQuantity) => oldQuantity + 1);
-    case MINUS_QUANTITY:
-      return changeQuantity(state, action, (oldQuantity) => oldQuantity - 1);
+    case UPDATE_INGREDIENTS:
+      const index = state.cartItems.findIndex(
+        ({ id }) => id === action.data.id
+      );
+      return produce(state, (draft) => {
+        draft.cartItems[index].ingredients = action.data.ingredients;
+      });
+    case UPDATE_QUANTITY:
+      return changeQuantity(state, action);
     case UPDATE_ITEMS_FROM_CART:
       const dishIndex = state.cartItems.findIndex(
         (item) =>
