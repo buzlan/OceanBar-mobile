f85690e91d4c13fbdc8d5f409532da60ec212796
igor <hotap70390@bio123.net>
2021-11-17 17:35:40 +0300

    create styles for new Screens

---
+++ src/styles/orderDeliveryScreenStyles.tsx
@@ -0,0 +1,74 @@
+import { StyleSheet } from "react-native";
+
+export const orderDeliveryScreenStyles = StyleSheet.create({
+  mainWrapper: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  wrapperWithoutDTPicker: {
+    paddingHorizontal: 20,
+    height: "100%",
+  },
+  selectDateWrapper: {
+    paddingTop: 40,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  dataElWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: 320,
+  },
+  dataText: {
+    fontSize: 24,
+    fontWeight: "400",
+    fontFamily: "Roboto",
+    color: "black",
+    paddingLeft: 20,
+  },
+  selectTimeWrapper: {
+    paddingTop: 70,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  selectPaidTypeWrapper: {
+    paddingTop: 70,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  buttonWrapper: {
+    paddingTop: 50,
+    paddingBottom: 30,
+  },
+  registerButton: {
+    alignContent: "center",
+    backgroundColor: "#FF4D00",
+    borderWidth: 1,
+    borderColor: "#FF4D00",
+    marginHorizontal: 20,
+    height: 50,
+  },
+  titleRegisterBtn: {
+    backgroundColor: "transparent",
+    color: "white",
+    fontFamily: "Roboto",
+    fontSize: 18,
+    fontWeight: "700",
+  },
+  disabledRegisterButton: {
+    alignContent: "center",
+    backgroundColor: "transparent",
+    borderWidth: 1,
+    borderColor: "#FF4D00",
+    marginHorizontal: 20,
+    height: 50,
+  },
+  disabledTitleRegisterBtn: {
+    backgroundColor: "transparent",
+    color: "#FF4D00",
+    fontFamily: "Roboto",
+    fontSize: 18,
+    fontWeight: "700",
+  },
+});
